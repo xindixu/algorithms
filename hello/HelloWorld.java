0e9a946dc77cee6073ab92665720435179b914ee
Xindi Xu <xindixu@utexas.edu>
2021-05-09 04:51:58 -0500

    clean up

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,9 +1,3 @@
-/* *****************************************************************************
- *  Name:              Ada Lovelace
- *  Coursera User ID:  123456
- *  Last modified:     October 16, 1842
- **************************************************************************** */
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
