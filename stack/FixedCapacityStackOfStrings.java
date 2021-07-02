83aea3a3ce92bc3bc1451595be3d518b1e394cb7
Xindi Xu <xindixu0@gmail.com>
2021-07-01 20:29:11 -0500

    feat(stack): array implementation with fixed capacity for strings

--- stack/FixedCapacityStackOfStrings.java
+++ stack/FixedCapacityStackOfStrings.java
@@ -1,26 +1,40 @@
-package com.stack;
+package stack;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class FixedCapacityStackOfStrings {
 
-    public boolean isEmpty() {
-        return false;
+    private final String[] array;
+    private int count;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        array = new String[capacity];
+        count = 0;
     }
-    public void push(String item){}
-    public String pop(){
-        return "";
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public void push(String item) {
+        array[count++] = item;
+    }
+
+    public String pop() {
+        return array[--count];
     }
 
     public static void main(String[] args) {
         int max = Integer.parseInt(args[0]);
         FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(max);
+        StdOut.println(max);
+
         while (!StdIn.isEmpty()) {
             String item = StdIn.readString();
             if (!item.equals("-")) stack.push(item);
-            else if (stack.isEmpty())  StdOut.println("BAD INPUT");
-            else                       StdOut.print(stack.pop() + " ");
+            else if (stack.isEmpty()) StdOut.println("BAD INPUT");
+            else StdOut.print(stack.pop() + " ");
         }
         StdOut.println();
 
