0e01cf4ebdefa405f02a3a01fd080d32b766e73e
Xindi Xu <xindixu0@gmail.com>
2021-08-16 22:26:28 -0500

    feat(kd-trees): implement contains methods of kd-tree

--- kd_trees/KdTree.java
+++ kd_trees/KdTree.java
@@ -3,6 +3,7 @@ package kd_trees;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class KdTree {
     private static final int CANVAS_SIZE = 1;
@@ -13,12 +14,14 @@ public class KdTree {
     private class Node {
         private double key;
         private Point2D value;
+        private boolean useX;
         private Node left;
         private Node right;
         private int size;
 
         public Node(Point2D point, boolean useX, int size) {
             this.key = useX ? point.x() : point.y();
+            this.useX = useX;
             this.value = point;
             this.size = size;
         }
@@ -61,7 +64,34 @@ public class KdTree {
         return cur;
     }
 
+    /**
+     * Does the tree contains this point p
+     * @param p the point to test
+     * @return does the set contain point p
+     */
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        return contains(root, p);
+    }
 
+    private boolean contains(Node cur, Point2D p) {
+        if (cur == null) return false;
+
+        StdOut.println(cur.value.toString());
+        if (cur.useX) {
+            if (cur.key < p.x()) return contains(cur.right, p);
+            if (cur.key > p.x()) return contains(cur.left, p);
+            return cur.value.y() == p.y();
+        } else {
+            if (cur.key < p.y()) return contains(cur.right, p);
+            if (cur.key > p.y()) return contains(cur.left, p);
+            return cur.value.x() == p.x();
+        }
+    }
+
+    /**
+     * Draw the kd-tree division
+     */
     public void draw() {
         draw(root, 0, CANVAS_SIZE, 0, CANVAS_SIZE, 1);
     }
@@ -79,19 +109,21 @@ public class KdTree {
 
         // draw the line
         StdDraw.setPenRadius();
-        if (cur.key == point.x()) {
+        if (cur.useX) {
             StdDraw.setPenColor(StdDraw.RED);
             StdDraw.line(cur.key, yMin, cur.key, yMax);
+            // further divide the current rectangular vertically
             draw(cur.left, xMin, cur.key, yMin, yMax, level + 1);
             draw(cur.right, cur.key, xMax, yMin, yMax, level + 1);
         } else {
             StdDraw.setPenColor(StdDraw.BLUE);
             StdDraw.line(xMin, cur.key, xMax, cur.key);
+            // further divide the current rectangular horizontally
             draw(cur.left, xMin, xMax, yMin, cur.key, level + 1);
             draw(cur.right, xMin, xMax, cur.key, yMax, level + 1);
         }
     }
-
+    
 
     public static void main(String[] args) {
         // read the n points from a file
@@ -104,8 +136,10 @@ public class KdTree {
             kdTree.insert(new Point2D(x, y));
         }
 
-
         kdTree.draw();
 
+        // does the tree contains these points
+        StdOut.println(kdTree.contains(new Point2D(0.09, 0.34)));
+        StdOut.println(kdTree.contains(new Point2D(0.09, 0.32)));
     }
 }
